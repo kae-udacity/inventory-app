a5ba4ee8d5a2e39954b020e2f29a301a89933ddb
Kenneth <kennethrogers.kr@gmail.com>
2017-11-14 16:40:11 +0100

    returned Cursor object from BookPresenter.readData

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,20 +1,49 @@
 package com.example.android.inventoryapp;
 
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.android.inventoryapp.data.BookContract;
+import com.example.android.inventoryapp.data.BookDbHelper;
 import com.example.android.inventoryapp.data.BookPresenter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private BookPresenter bookPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BookPresenter bookPresenter = new BookPresenter(this);
+        bookPresenter = new BookPresenter(new BookDbHelper(this));
         bookPresenter.insertBook("The Martian", 1199, "Del Rey", 555923391);
         bookPresenter.insertBook("Lord of the Flies", 899, "Faber", 555978057);
-        bookPresenter.readData();
+        readData();
+    }
+
+    private void readData() {
+        String[] projection = {
+                BookContract.BookEntry._ID,
+                BookContract.BookEntry.COLUMN_PRODUCT_NAME,
+                BookContract.BookEntry.COLUMN_PRODUCT_PRICE,
+                BookContract.BookEntry.COLUMN_PRODUCT_QUANTITY,
+                BookContract.BookEntry.COLUMN_PRODUCT_SUPPLIER_NAME,
+                BookContract.BookEntry.COLUMN_PRODUCT_SUPPLIER_EMAIL,
+                BookContract.BookEntry.COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER,
+        };
+
+        Cursor cursor = bookPresenter.readData(projection);
+        try {
+            if (cursor.moveToFirst()) {
+                Log.i(TAG, "Cursor: " + DatabaseUtils.dumpCursorToString(cursor));
+            }
+        } finally {
+            cursor.close();
+        }
     }
 }
--- app/src/main/java/com/example/android/inventoryapp/data/BookPresenter.java
+++ app/src/main/java/com/example/android/inventoryapp/data/BookPresenter.java
@@ -1,10 +1,8 @@
 package com.example.android.inventoryapp.data;
 
 import android.content.ContentValues;
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 /**
  * Handles the updating and reading of data from the inventory database.
@@ -12,12 +10,10 @@ import android.util.Log;
 
 public class BookPresenter {
 
-    private static final String TAG = BookPresenter.class.getSimpleName();
-
     private BookDbHelper bookDbHelper;
 
-    public BookPresenter(Context context) {
-        bookDbHelper = new BookDbHelper(context);
+    public BookPresenter(BookDbHelper bookDbHelper) {
+        this.bookDbHelper = bookDbHelper;
     }
 
     public void insertBook(String name, int priceInCents, String supplierName, int supplierPhoneNumber) {
@@ -31,49 +27,8 @@ public class BookPresenter {
         database.insert(BookContract.BookEntry.TABLE_NAME, null, values);
     }
 
-    public void readData() {
+    public Cursor readData(String[] projection) {
         SQLiteDatabase database = bookDbHelper.getReadableDatabase();
-
-        String[] projection = {
-                BookContract.BookEntry._ID,
-                BookContract.BookEntry.COLUMN_PRODUCT_NAME,
-                BookContract.BookEntry.COLUMN_PRODUCT_PRICE,
-                BookContract.BookEntry.COLUMN_PRODUCT_QUANTITY,
-                BookContract.BookEntry.COLUMN_PRODUCT_SUPPLIER_NAME,
-                BookContract.BookEntry.COLUMN_PRODUCT_SUPPLIER_EMAIL,
-                BookContract.BookEntry.COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER,
-        };
-
-        Cursor cursor = database.query(BookContract.BookEntry.TABLE_NAME, projection, null, null, null, null, null);
-
-        try {
-            int idColumnIndex = cursor.getColumnIndex(BookContract.BookEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_PRODUCT_NAME);
-            int priceColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_PRODUCT_PRICE);
-            int quantityColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_PRODUCT_QUANTITY);
-            int supplierNameColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_PRODUCT_SUPPLIER_NAME);
-            int supplierEmailColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_PRODUCT_SUPPLIER_EMAIL);
-            int supplierPhoneNumberColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER);
-
-            while (cursor.moveToNext()) {
-                int currentId = cursor.getInt(idColumnIndex);
-                String currentName = cursor.getString(nameColumnIndex);
-                int currentPrice = cursor.getInt(priceColumnIndex);
-                int currentQuantity = cursor.getInt(quantityColumnIndex);
-                String currentSupplierName = cursor.getString(supplierNameColumnIndex);
-                String currentSupplierEmail = cursor.getString(supplierEmailColumnIndex);
-                int currentSupplierPhoneNumber = cursor.getInt(supplierPhoneNumberColumnIndex);
-
-                Log.i(TAG, "ID: " + currentId +
-                        ", name: " + currentName +
-                        ", price: " + currentPrice +
-                        ", quantity: " + currentQuantity +
-                        ", supplier name: " + currentSupplierName +
-                        ", supplier email: " + currentSupplierEmail +
-                        ", supplier phone number: " + currentSupplierPhoneNumber);
-            }
-        } finally {
-            cursor.close();
-        }
+        return database.query(BookContract.BookEntry.TABLE_NAME, projection, null, null, null, null, null);
     }
 }
